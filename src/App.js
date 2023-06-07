703133d06bc50b56515cf25ba06e45188607e359
RachelChae <rachel_chae@brown.edu>
2023-06-07 19:50:34 -0400

    partial

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <NavbarComp/>
+      <h>
+        hello
+      </h>
     </div>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_image from '../assets/profile_pic_name.png';
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_pic"
+          // Image goes here
+          alt="Profile Pic" src={profile_image} style ={{width: 380, height: 500}}
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Rachel Chae</div>
+        <div className="brief_description">
+          Hi, I'm a junior studying Computer Science @ Brown University!
+        </div>
+      </div>
+    </div>
+  </div>
+
       </div>
     )
   }
